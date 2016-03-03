1dffb772eb996201e766480ad4a5718fa6f86b3e
Lynsey Teague <lynsey.teague@yahoo.com>
2016-03-03 06:32:19 -0500

    ngsubmit and reset
    issues with reset, not working yet

--- js/myApp.js
+++ js/myApp.js
@@ -1,12 +1,42 @@
+angular.module('myApp', ['ngMessages']);
 angular.module('myApp', [])
     .controller('wordController', ['$scope', function ($scope) {
-        $scope.femaleName = "female name";
-        $scope.dirtyTask = "dirty task";
-        $scope.obnoxiousCelebrity = "obnoxious celebrity";
-        $scope.jobTitle = "job title";
-        $scope.celebrity = "celebrity";
-        $scope.hugeNumber = "huge number'";
-        $scope.tediousTask = "tedious task";
-        $scope.uselessSkill = "useless skill";
-        $scope.adjective = "adjective";
+
+
+        //hide and show sections
+        $scope.inputForm = true;
+        $scope.submit = function () {
+            if ($scope.myForm.$valid) {
+                $scope.inputForm = false;
+                $scope.results = true;
+            } else {
+                $scope.inputForm = true;
+                $scope.results = false;
+            }
+        }
+
+
+        // Set the following in your controller for the form/page.
+        // Allows you to set default form values on fields.
+        $scope.defaultFormData = {
+                yourName: "",
+                dirtyTask: "",
+                obnoxiousCelebrity: "",
+                jobTitle: "",
+                celebrity: "",
+                hugeNumber: "",
+                tediousTask: "",
+                uselessSkill: "",
+                adjective: ""
+            }
+            // Save a copy of the defaultFormData
+        $scope.resetCopy = angular.copy($scope.defaultFormData);
+        // Create a method to reset the form back to it's original state.
+        $scope.resetForm = function () {
+            // Set the field values back to the original default values
+            $scope.defaultFormData = angular.copy($scope.resetCopy);
+            $scope.myForm.$setPristine();
+            $scope.myForm.$setValidity();
+            $scope.myForm.$setUntouched();
+        }
     }]);
